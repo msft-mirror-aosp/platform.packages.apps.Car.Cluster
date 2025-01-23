dc863a75d54f5bd70f76f9dc11cd4cafb09215f0
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-01-22 17:00:16 -0800

    Revert "Show the cluster activity when the screen is locked."
    This revert was created by Android Culprit Assistant. The culprit was identified in the following culprit search session (http://go/aca-get/ca91afe2-f7dc-4219-ba94-2aba8da8a1cf).
    
    Bug: 389202316
    Change-Id: I8c9903446f5828a3635d0460589a62222483465a
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- ClusterHomeSample/src/com/android/car/cluster/home/ClusterHomeActivityLightMode.java
+++ ClusterHomeSample/src/com/android/car/cluster/home/ClusterHomeActivityLightMode.java
@@ -27,7 +27,6 @@ public class ClusterHomeActivityLightMode extends ClusterHomeActivity {
     private static final long HEARTBEAT_INTERVAL_MS = 1000; // 1 second interval.
 
     private ClusterHomeManager mClusterHomeManager;
-    private String mText;
 
     private final Runnable mSendHeartbeatsRunnable = () -> sendHeartbeats();
 
@@ -45,8 +44,6 @@ public class ClusterHomeActivityLightMode extends ClusterHomeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mText = getResources().getString(R.string.cluster_home_text) + " (LIGHT mode)";
-        mTextView.setText(mText);
 
         Car car = Car.createCar(getApplicationContext());
         mClusterHomeManager = (ClusterHomeManager) car.getCarManager(ClusterHomeManager.class);
@@ -72,9 +69,7 @@ public class ClusterHomeActivityLightMode extends ClusterHomeActivity {
     }
 
     private void sendHeartbeats() {
-        long nanoTime = System.nanoTime();
-        mTextView.setText(mText + "\nsendHeartbeat: " + nanoTime);
-        mClusterHomeManager.sendHeartbeat(nanoTime, /* appMetadata= */ null);
+        mClusterHomeManager.sendHeartbeat(System.nanoTime(), /* appMetadata= */ null);
 
         getMainThreadHandler().postDelayed(mSendHeartbeatsRunnable, HEARTBEAT_INTERVAL_MS);
     }
