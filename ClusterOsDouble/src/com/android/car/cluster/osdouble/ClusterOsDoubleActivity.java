dce23fd969826688d3a2eca9e20e8c1bf2f6e00b
Antonio Kantek <kanant@google.com>
2025-04-08 19:36:37 -0700

    Revert "Use trusted and top focus disabled virtual display for ClusterOsDouble"
    This reverts commit 4e35df30b2592abc684b9aa5a9140e098a8db53e.
    
    Reason for revert: Broke
    CtsInputMethodTestCases:EditTextImeSupportTest#testDisableImeAfterFocusChange
    on MUMD (see `CtsInputMethodTestCases for driver` at
    go/aaos-cts-presubmit#ime-targets ,starting on build ab/13322875).
    
    Bug: 409298179
    Test: https://android-build.corp.google.com/abtd/run/L00800030011056337
    Change-Id: I152dd202c094c552079eaae873e9a8603c4f7f56

--- ClusterOsDouble/src/com/android/car/cluster/osdouble/ClusterOsDoubleActivity.java
+++ ClusterOsDouble/src/com/android/car/cluster/osdouble/ClusterOsDoubleActivity.java
@@ -20,8 +20,6 @@ import static android.car.VehicleAreaType.VEHICLE_AREA_TYPE_GLOBAL;
 import static android.car.cluster.ClusterHomeManager.UI_TYPE_CLUSTER_HOME;
 import static android.car.cluster.ClusterHomeManager.UI_TYPE_CLUSTER_NONE;
 import static android.hardware.display.DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY;
-import static android.hardware.display.DisplayManager.VIRTUAL_DISPLAY_FLAG_STEAL_TOP_FOCUS_DISABLED;
-import static android.hardware.display.DisplayManager.VIRTUAL_DISPLAY_FLAG_TRUSTED;
 
 import static com.android.car.cluster.osdouble.ClusterOsDoubleApplication.TAG;
 
@@ -207,12 +205,9 @@ public class ClusterOsDoubleActivity extends ComponentActivity {
                 + "x" + height);
         return mDisplayManager.createVirtualDisplay(/* projection= */ null, "ClusterOsDouble-VD",
                 width, height, 160, surface,
-                // Use VIRTUAL_DISPLAY_FLAG_STEAL_TOP_FOCUS_DISABLED, because we don't want the
-                // cluster display to be the focus display which can hinder Rotary service
-                // (b/206862329). VIRTUAL_DISPLAY_FLAG_TRUSTED is needed so that we get display
-                // area callbacks.
-                VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY | VIRTUAL_DISPLAY_FLAG_TRUSTED
-                        | VIRTUAL_DISPLAY_FLAG_STEAL_TOP_FOCUS_DISABLED,
+                // Don't use VIRTUAL_DISPLAY_FLAG_TRUSTED, because we don't want the cluster display
+                // to be the focus display which can hinder Rotary service (b/206862329).
+                VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY,
                 /* callback= */ null, /* handler= */ null, "ClusterDisplay");
     }
 
